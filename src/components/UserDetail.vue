346b164e819aaf0867d071046670615ec0e4b1ce
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-26 11:06:35 -0600

    Added property type validation and a method to reverse the characters in name.

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,20 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: {
+            name: String
+        },
+        methods: {
+            switchName(){
+                return this.name.split("").reverse().join("");
+            }
+        }
     }
 </script>
 
