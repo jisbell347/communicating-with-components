914db4bf56913ef97222607eec595a25e2010abb
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-26 17:13:12 -0600

    Created a resetName method which uses a custom event listener. Used type validation.

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,17 +3,26 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            name: String
+            name: {
+                type:String
+            },
+            resetFn: Function
         },
         methods: {
             switchName(){
                 return this.name.split("").reverse().join("");
+            },
+            resetName(){
+                this.name = 'Joe';
+                this.$emit('nameWasReset', this.name);
             }
         }
     }
